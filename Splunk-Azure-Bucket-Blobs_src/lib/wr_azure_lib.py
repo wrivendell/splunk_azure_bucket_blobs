6a17b9eb1b057dfaeefd5c24c4143f072cfb2d45
will <RootP@ss?1>
2021-06-03 03:20:00 -0700

    updated wazure to filter on container name blob pulls

--- Splunk-Azure-Bucket-Blobs_src/lib/wr_azure_lib.py
+++ Splunk-Azure-Bucket-Blobs_src/lib/wr_azure_lib.py
@@ -173,33 +173,33 @@ class BlobService():
 		Probes each container for blobs contained and writes all out to a list of dicts. One dict per container
 		containing container info plus an embedded list of dicts continaing all blobs info
 		'''
-		try:
-			tmp_container_blob_dict_list = []
-			all_containers_dict_list = self.getContainers()
-			for container in all_containers_dict_list:
-				print("- WAZURE(" + str(sys._getframe().f_lineno) +"): Processing: " + container['name'] + " -")
-				found = True
-				if container_name_list:
-					found = False
-					for i in container_name_list:
-						if not i in container['name']:
-							continue
-						else:
-							found = True
-							break
-				if not found:
-					print("- WAZURE(" + str(sys._getframe().f_lineno) +"): " + container['name'] + " Not in list, skipping. -")
-					continue
-				else:
-					print("- WAZURE(" + str(sys._getframe().f_lineno) +"): " + container['name'] + " Added -")
-				tmp_blobs_dict_list = self.getBlobsByContainer(container['name'])
-				tmp_container_dict = container
-				tmp_container_dict['blobs'] = (tmp_blobs_dict_list)
-				tmp_container_blob_dict_list.append(tmp_container_dict)
-			return(tmp_container_blob_dict_list)
-		except Exception as ex:
-			print("- WAZURE(" + str(sys._getframe().f_lineno) +"): Exception: -")
-			print(ex)
+		#try:
+		tmp_container_blob_dict_list = []
+		all_containers_dict_list = self.getContainers()
+		for container in all_containers_dict_list:
+			print("- WAZURE(" + str(sys._getframe().f_lineno) +"): Processing: " + container['name'] + " -")
+			found = True
+			if container_name_list:
+				found = False
+				for i in container_name_list:
+					if not i in container['name']:
+						continue
+					else:
+						found = True
+						break
+			if not found:
+				print("- WAZURE(" + str(sys._getframe().f_lineno) +"): " + container['name'] + " Not in list, skipping. -")
+				continue
+			else:
+				print("- WAZURE(" + str(sys._getframe().f_lineno) +"): " + container['name'] + " Added -")
+			tmp_blobs_dict_list = self.getBlobsByContainer(container['name'])
+			tmp_container_dict = container
+			tmp_container_dict['blobs'] = (tmp_blobs_dict_list)
+			tmp_container_blob_dict_list.append(tmp_container_dict)
+		return(tmp_container_blob_dict_list)
+	#	except Exception as ex:
+	#		print("- WAZURE(" + str(sys._getframe().f_lineno) +"): Exception: -")
+	#		print(ex)
 	
 	def downloadBlobByName(self, blob_name:str, expected_blob_size:int, container_name:str, dest_download_loc_root='./blob_downloads/', bypass_size_compare=False, timeout=5000) -> list:
 		'''
