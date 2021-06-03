fc4843edf57584ac49c6de6bb19707976e28ae33
will <RootP@ss?1>
2021-06-03 04:11:14 -0700

    blob processing output

--- Splunk-Azure-Bucket-Blobs_src/lib/wr_azure_lib.py
+++ Splunk-Azure-Bucket-Blobs_src/lib/wr_azure_lib.py
@@ -140,8 +140,8 @@ class BlobService():
 			tmp_blob_list = []
 			container_client = blob_service_client.get_container_client( (container_name) )
 			blob_list = container_client.list_blobs()
-#			print("- WAZURE(" + str(sys._getframe().f_lineno) +"): Amount of BLOBS: " + str(len(blob_list)) + " -")
-			print(blob_list)
+			tmp_blist = list(blob_list)
+			print("- WAZURE(" + str(sys._getframe().f_lineno) +"): Amount of BLOBS: " + str(len(tmp_blist)) + " -")
 			for blob in blob_list:
 				if names_only:
 					tmp_blob_list.append( blob['name'] )
